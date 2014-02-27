c7dedf991105b3b6f269e4e6774f114f3b1ec443
zhangchile <zzlchile@gmail.com>
2014-02-27 09:43:24 +0800

    format view.php and viewall.php;

--- application/views/items/viewall.php
+++ application/views/items/viewall.php
@@ -2,13 +2,12 @@
 <input type="text" value="I have to..." onclick="this.value=''" name="todo"> <input type="submit" value="add">
 </form>
 <br/><br/>
-<?php $number = 0?>
- 
+<?php $number = 0;?>
 <?php foreach ($todo as $todoitem):?>
-    <a class="big" href="../items/view/<?php echo $todoitem['Item']['id']?>/<?php echo strtolower(str_replace(" ","-",$todoitem['Item']['item_name']))?>">
-    <span class="item">
-    <?php echo ++$number?>
-    <?php echo $todoitem['Item']['item_name']?>
-    </span>
-    </a><br/>
+    <a class="big" href="../items/view/<?php echo $todoitem['Item']['id'];?>/<?php echo strtolower(str_replace(" ","-",$todoitem['Item']['item_name']));?>">
+    <span class="item">
+    <?php echo ++$number;?>
+    <?php echo $todoitem['Item']['item_name'];?>
+    </span>
+    </a><br/>
 <?php endforeach?>
